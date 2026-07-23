6b6d55f13081ed9ea10441aea94e050a4c64f426
sobhantk48 <sobhantk48@gmail.com>
2026-07-23 22:00:04 +0330

    fix: remove legacy Android native sources from phase 1 build

---
+++ android/app/src/main/kotlin/com/example/v2ray_stk/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.v2ray_stk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/v2raystk/v2ray_stk/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.v2raystk.v2ray_stk
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
